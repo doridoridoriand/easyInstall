7c6a488c61170a29f00473d76be7918da72e4f61
Dorian <i.am.eager.for.peace@gmail.com>
2014-05-19 18:28:18 +0900

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,14 @@
-/usr/bin/zsh
+#!/usr/bin/env bash
 
-echo ----installing and vim ----
-sudo apt-get install vim
-
-echo ----installing zshrc and vimrc----
 cd $HOME
-git clone https://github.com/unosk/dotfiles.git
-cd dotfiles
-./install.sh
+sudo apt-get update
+sudo apt-get -y upgrade
 
-echo ----installing ruby with rbenv and rails----
-cd $HOME
-sudo sudo apt-get install curl g++
-sudo apt-get install zlib1g-dev libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt-dev
-sudo apt-get install sqlite3 libsqlite3-dev
+sudo apt-get -y install git
+sudo sudo apt-get -y install curl g++
+sudo apt-get -y install zlib1g-dev libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt-dev
+sudo apt-get -y install sqlite3 libsqlite3-dev
+
 cd $HOME
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
@@ -22,21 +17,17 @@ exec $SHELL
 mkdir -p ~/.rbenv/plugins
 cd ~/.rbenv/plugins
 git clone git://github.com/sstephenson/ruby-build.git
-sudo aptitude install make
+sudo aptitude -y install make
 rbenv install 2.1.1
 rbenv rehash
 rbenv global 2.1.1
 gem install rails --version="~>4.0"
 rbenv rehash
 
-echo ----installing node.js and grunt-cli----
+sudo apt-get -y install vim
+sudo apt-get -y install zsh
+
 cd $HOME
-git clone https://github.com/creationix/nvm .nvm
-cd .nvm/
-source ~/.nvm/nvm.sh
-echo nvm use default >> ~/.zshrc
-echo npm_dir=${NVM_PATH}_modules >> ~/.zshrc
-echo export NODE_PATH=$npm_dir >> ~/.zshrc
-export NODE_PATH=${NVM_PATH}_modules
-nvm install v0.10.24
-npm install -g grunt-cli
+git clone https://github.com/unosk/dotfiles.git
+cd dotfiles
+./install.sh
